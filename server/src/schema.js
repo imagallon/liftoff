a7b7250a595c3cc853cf98561cf1e026305ed2b5
Izzy <browncans@gmail.com>
2023-10-09 23:38:54 -0700

    scheme type Author and first query for tracks

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,9 +1,15 @@
 const gql = require("graphql-tag");
 
 const typeDefs = gql`
+
+type Query {
+  "Get tracks array for homepage grid"
+  tracksForHome: [Track!]!
+}
+
 "A Track is a group of Modules that the teaches about a specific topic"
 type Track {
-  
+
   id: ID!
   "the track's title"
   title: String!
@@ -15,7 +21,13 @@ type Track {
   length: Int
   "the number of modules this track contains"
   modulesCount: Int
+}
 
+"Author of a complete Track or a Module"
+type Author {
+  id: ID!
+  name: String!
+  photo: String
 }
 
 
